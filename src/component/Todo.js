f6a1662deb065bda795c8624d9ff9c9855b89a84
Enzo Esperguin <enzoesper@hotmail.com>
2019-09-11 01:11:53 -0300

    adding firebase connection to post todos with axios and also fetch the data stored

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,8 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Todo = () => {
   const [todoName, setTodoName] = useState("");
   const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://todo-list-28c94.firebaseio.com/todos.json")
+      .then(result => {
+        console.log(result);
+        const todoData = result.data;
+        const todos = [];
+        for (const key in todoData) {
+          todos.push({ id: key, name: todoData[key].name });
+        }
+        setTodoList(todos);
+      });
+  }, []);
 
   const inputChangeHandler = e => {
     setTodoName(e.target.value);
@@ -10,6 +25,16 @@ const Todo = () => {
 
   const todoAddHandler = () => {
     setTodoList(todoList.concat(todoName));
+    axios
+      .post("https://todo-list-28c94.firebaseio.com/todos.json", {
+        name: todoName
+      })
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   return (
@@ -25,7 +50,7 @@ const Todo = () => {
       </button>
       <ul>
         {todoList.map(todo => (
-          <li key={todo}>{todo}</li>
+          <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
     </React.Fragment>
